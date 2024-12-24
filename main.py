3946bb7c60b6e74f8471ceb189c58706cb0e908a
Ivan Diaz <phi.designer@gmail.com>
2024-12-24 11:12:46 +0000

    Succefully ran test protobuf==3.20.3

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ _steps = [
     # NOTE: We do not include this in the steps so it is not run by mistake.
     # You first need to promote a model export to "prod" before you can run this,
     # then you need to run this step explicitly
-    #    "test_regression_model"
+    # "test_regression_model"
 ]
 
 
@@ -117,10 +117,8 @@ def go(config: DictConfig):
 
         if "test_regression_model" in active_steps:
             _ = mlflow.run(
-                (
-                    f"{config['main']['components_repository']}"
-                    f"/test_regression_model"
-                ),
+                os.path.join(hydra.utils.get_original_cwd(),
+                             "components", "test_regression_model"),
                 "main",
                 parameters={
                     "mlflow_model": "random_forest_export:prod",
